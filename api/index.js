8055ab71a5c0a39d6a07b2ba767dab17b7191768
Mansi <mansidiwakar467@gmail.com>
2024-12-09 19:22:54 +0530

    create database

--- api/index.js
+++ api/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("MongoDb is connected");
-});
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
